8afee54474b74839fd740eff6c1c40b3a92b609b
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2024-08-26 09:46:06 -0300

    kj

--- login.php
+++ login.php
@@ -6,14 +6,14 @@ require_once 'vendor/autoload.php';
 require_once "admin/src/ClienteDAO.php";
 require_once "admin/src/ClienteLogDAO.php";
 
-$guzzleClient = new \GuzzleHttp\Client(array( 'curl' => array( CURLOPT_SSL_VERIFYPEER => false, ), ));
+$guzzleClient = new \GuzzleHttp\Client(array('curl' => array(CURLOPT_SSL_VERIFYPEER => false,),));
 // Configurações da API do Google
 $client = new Google\Client();
 $client->setHttpClient($guzzleClient);
 
-$client->setClientId('142057793318-kp5hvaj401ho2vn1bai0ipaqkk94edip.apps.googleusercontent.com'); 
-$client->setClientSecret('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'); 
-$client->setRedirectUri('http://localhost:8000/login.php'); 
+$client->setClientId('142057793318-kp5hvaj401ho2vn1bai0ipaqkk94edip.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('http://localhost:8000/login.php');
 $client->addScope('email');
 $client->addScope('profile');
 
@@ -133,6 +133,7 @@ if (isset($_POST['login'])) {
             <div class="title_container">
                 <p class="title">Entre em sua conta!</p>
                 <span class="subtitle">Realize seu login e usufrua de todos os beneficios do nosso site!</span>
+                <span class="subtitle">Voce ainda não possui uma conta? <a href="/cadastro.php" class="text-decoration-none">Cadastrar</a></span>
             </div>
             <br>
             <div class="input_container">
@@ -158,7 +159,7 @@ if (isset($_POST['login'])) {
 
             <div class="separator">
                 <hr class="line">
-                <span>Or</span>
+                <span>Ou</span>
                 <hr class="line">
             </div>
             <a href="<?= htmlspecialchars($loginUrl) ?>" class="sign-in_ggl text-decoration-none">
@@ -176,7 +177,12 @@ if (isset($_POST['login'])) {
                         <path fill="#4285f4" clip-path="url(#B)" d="M48 48L17 24l-4-3 35-10z"></path>
                     </g>
                 </svg>
-                <span>Sign In with Google</span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" class="me-1" height="20" viewBox="0 0 48 48">
+                        <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                        <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
+                        <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path>
+                        <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                    </svg>Entrar com o Google</span>
             </a>
             <?php
             if (isset($_SESSION['msg'])):
